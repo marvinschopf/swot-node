6f2c7666dc0a7294be0c2973b12191dd6d84cd1c
MagicMarvMan <marvin.kai@gmx.de>
2021-01-19 16:48:06 +0100

    Add documentation

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,26 @@ import * as fs from "fs";
 import academicTlds from "./academicTlds";
 import * as path from "path";
 
+// Remove public suffixes from the domain
 function domainWithoutSuffix(domain: string, publicSuffix: string): string {
 	return domain.replace("." + publicSuffix, "");
 }
 
 export function isAcademic(url: string): boolean {
+	// Parse the URL using TLDjs
 	const parsedUrl: any = parse(url);
+
+	// Check if the TLD is an academic TLD
 	if (academicTlds.indexOf(parsedUrl.publicSuffix) > -1) {
 		return true;
 	}
+
+	// Check how many TLD's the suffix consists of
 	if (parsedUrl.publicSuffix.split(".").length > 1) {
+		// If the suffix consists of multiple domains, split them into an array and reverse it
 		const suffixes: Array<string> = parsedUrl.publicSuffix.split(".").reverse();
+
+		// Check if the file of the institution exists
 		if (
 			fs.existsSync(
 				path.resolve(
@@ -31,6 +40,7 @@ export function isAcademic(url: string): boolean {
 			return false;
 		}
 	} else {
+		// If the suffix consists of only one domain, it can be searched for without making any further changes to the suffix.
 		if (
 			fs.existsSync(
 				path.resolve(
