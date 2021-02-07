945beb5d9316bfdf93990d527a0b27454d0bbb5a
Marvin Schopf <marvin@schopf.biz>
2021-02-07 15:51:31 +0100

    Use random values from crypto module

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,19 +1,13 @@
 import { expect } from "chai";
 import { isAcademic, getSchoolName, getSchoolNames } from "./../src";
+import { randomBytes } from "crypto";
 
 import * as path from "path";
 import * as fs from "fs";
 import * as assert from "assert";
 
 function makeid(length: number): string {
-	let result: string = "";
-	const characters: string =
-		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-	const charactersLength: number = characters.length;
-	for (let i: number = 0; i < length; i++) {
-		result += characters.charAt(Math.floor(Math.random() * charactersLength));
-	}
-	return result;
+	return randomBytes(length).toString("hex");
 }
 
 function getDomainFile(domainPath: string): string {
